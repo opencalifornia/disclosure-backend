e4dfbcf388a9c842d34fe28d590bf74bf232c13d
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-23 01:45:29 -0800

    Add FIPS admin

--- fips/urls.py
+++ fips/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from django.contrib import admin
 
 from . import views
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^search/', views.search_view),
